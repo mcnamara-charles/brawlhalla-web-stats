d9e4f8283252cf030e664c8d5e839aa495b58c2e
mcnamara-charles <c.w.mcnamara@vikes.csuohio.edu>
2022-02-09 18:26:43 -0500

    Latest working version with Player Stats

--- src/components/PlayersPage.js
+++ src/components/PlayersPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
 import { Link, useLocation } from "react-router-dom";
+const _ = require("lodash");  
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -8,7 +9,9 @@ function useQuery() {
 
 function PlayersPage() {
 
-    const [player, setPlayer] = React.useState("")
+    const [player, setPlayer] = React.useState("");
+    const [playerRanked, setPlayerRanked] = React.useState("");
+
 
     let query = useQuery();
     let playerID = query.get("id")
@@ -16,7 +19,22 @@ function PlayersPage() {
     React.useEffect(() => {
         async function fetchData() {
             const data = (await fetch(`https://api.brawlhalla.com/player/${playerID}/stats?api_key=9WFVHV2XL7KRRNCIMULK`).then((response) => response.json()))
+            const Rdata = (await fetch(`https://api.brawlhalla.com/player/${playerID}/ranked?api_key=9WFVHV2XL7KRRNCIMULK`).then((response) => response.json()))
+            
+            // Convert Same Objects below for _.merge.
+            Rdata.ranked_wins = Rdata.wins;
+            Rdata.ranked_games = Rdata.games;
+
+            for(let i = 0; i<Rdata.legends.length; i++) {
+                Rdata.legends[i].ranked_wins = Rdata.legends[i].wins;
+                Rdata.legends[i].ranked_games = Rdata.legends[i].games;
+            }
+
+            const fullData = _.merge(data, Rdata)
+            
             setPlayer(data)
+            setPlayerRanked(Rdata)
+            console.log(data)
         }
            
         fetchData()
