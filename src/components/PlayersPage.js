8afc6235cae755452e4748a5489193680e4de4cf
mcnamara-charles <c.w.mcnamara@vikes.csuohio.edu>
2022-02-09 17:23:39 -0500

    Started player page

--- src/components/PlayersPage.js
+++ src/components/PlayersPage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
+import { Link, useLocation } from "react-router-dom";
 
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 
 function PlayersPage() {
+    let query = useQuery();
+    let playerID = query.get("id")
     return (
         <>
+            <div className='player'>
+
+            </div>
         </>
     );
 }
